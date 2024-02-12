60778b7cd0cd0efc9d18e6e4a06d71367cd0a86f
superfortune <sfortunepassion0908@outlook.com>
2024-02-12 09:37:45 +0000

    feat: utils.ts update add YAML dump and command helpers

--- src/utils.ts
+++ src/utils.ts
@@ -9,3 +9,10 @@ export function decapitalize(s: string): string {
     if (!s) return s;
     return `${s[0].toLowerCase()}${s.substr(1)}`;
 }
+
+export function getCommandName(): string {
+    return (process.argv[1] && process.argv[1].split("/").pop()) || "ghactions";
+}
+
+export function getCommandArgs(): string {
+    return process.argv.slice(2).join(" ");
