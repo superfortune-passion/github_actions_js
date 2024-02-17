1e89e6efdf12153ae9075d35d8c0b51cf0002c19
superfortune <sfortunepassion0908@outlook.com>
2024-02-17 09:15:43 +0000

    feat: urlUtils.ts improve add URL parsing and download helpers

--- src/urlUtils.ts
+++ src/urlUtils.ts
@@ -8,3 +8,13 @@ import { promisify } from "util";
 
 import { UTF8 } from "./constants.js";
 import { getTempDir } from "./utils.js";
+
+export async function download(url: string): Promise<string> {
+    const parsedUrl = new URL(url);
+    if (parsedUrl.protocol === "file:") {
+        return promisify(fs.readFile)(fileURLToPath(url), { encoding: UTF8 });
+    }
+    const tempPath = getTempDir();
+    const dest = path.join(tempPath, path.basename(url));
+    const file = fs.createWriteStream(dest);
+    const getFunc = parsedUrl.protocol === "https:" ? https.get : http.get;
