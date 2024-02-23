bfa1f35ea407e3e8d9538127bdb12951604acea1
superfortune <sfortunepassion0908@outlook.com>
2024-02-23 09:17:51 +0000

    feat: github.ts improve add GitHub API workflow listing

--- src/github.ts
+++ src/github.ts
@@ -5,3 +5,10 @@ import { URL } from "url";
 interface IResponse {
     message: string;
 }
+
+async function api(
+    hostname: string,
+    endpoint: string,
+    token?: string
+): Promise<unknown> {
+    const response = await fetch(`https://api.${hostname}/repos/${endpoint}`, {
