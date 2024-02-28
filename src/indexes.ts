23e9d1d9aa863ccd7d95a47f80ffcbfbb1853b88
superfortune <sfortunepassion0908@outlook.com>
2024-02-28 12:11:24 +0000

    feat: indexes.ts scaffold add workflow index registry

---
+++ src/indexes.ts
@@ -0,0 +1,7 @@
+export const JS_INDEX_URL =
+    "https://github.com/vemel/github_actions_js/tree/main/nodejs_workflows";
+
+export interface IIndex {
+    url: string;
+    title: string;
+    shortcut: string;
