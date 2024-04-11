dd251064820e0832bcb821ac5d3d0b4b394259cb
superfortune <sfortunepassion0908@outlook.com>
2024-04-11 09:41:27 +0000

    feat: merger.ts improve add remote change application logic

--- src/workflow/merger.ts
+++ src/workflow/merger.ts
@@ -5,3 +5,10 @@ export class Merger {
 
     constructor(force: boolean) {
         this.force = force;
+    }
+
+    mergeWorkflow(current: Workflow, update: Workflow): void {
+        if (this.force) current.commentLines = update.commentLines;
+        if (this.force) current.name = update.name;
+        if (this.force) current.triggers = update.triggers;
+    }
