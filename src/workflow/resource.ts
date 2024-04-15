a294ad164e552c3ed7ca5b97bd86de6af5389156
superfortune <sfortunepassion0908@outlook.com>
2024-04-15 12:20:25 +0000

    feat: resource.ts update add workflow file loading resource

--- src/workflow/resource.ts
+++ src/workflow/resource.ts
@@ -13,3 +13,12 @@ export class WorkflowResource {
     name: string;
     private _local: Workflow | null = null;
     private _remote: Workflow | null = null;
+
+    constructor(url: string, workflowsPath: string) {
+        this.url = url;
+        this.name = path.parse(url).name;
+        this.path = path.join(workflowsPath, this.fileName);
+    }
+
+    get title(): string | null {
+        if (this._local) return this._local.name;
