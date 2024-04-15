ad3abfcff349288d4d8c1bba521dc2363151037d
superfortune <sfortunepassion0908@outlook.com>
2024-04-15 09:59:10 +0000

    feat: resource.ts improve add workflow file loading resource

--- src/workflow/resource.ts
+++ src/workflow/resource.ts
@@ -6,3 +6,10 @@ import { promisify } from "util";
 import { UTF8 } from "../constants.js";
 import { download } from "../urlUtils.js";
 import { Workflow } from "./workflow.js";
+
+export class WorkflowResource {
+    path: string;
+    url: string;
+    name: string;
+    private _local: Workflow | null = null;
+    private _remote: Workflow | null = null;
