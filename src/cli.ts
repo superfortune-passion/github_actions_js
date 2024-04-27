8baf457d13130f30b42234bd4dfb3aec4427673a
superfortune <sfortunepassion0908@outlook.com>
2024-04-27 12:28:37 +0000

    feat: cli.ts improve add CLI argument parsing and help

--- src/cli.ts
+++ src/cli.ts
@@ -13,3 +13,18 @@ export interface Namespace {
     force: boolean;
     update: boolean;
     list: boolean;
+    diff: boolean;
+    clean: boolean;
+    index: string;
+}
+
+export function getHelp(): string {
+    return commandLineUsage([
+        {
+            header: "GitHub Actions Manager",
+            content: [
+                "CLI tool to install and update and share GitHub Actions workflows",
+                "",
+                `Documentation: ${DOCS_URL}`,
+                "",
+                "{bold ghactions} - Run in interactive mode",
