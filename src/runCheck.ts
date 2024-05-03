ab811e69d5d0ff90764d31bf61698fee5e7f3658
superfortune <sfortunepassion0908@outlook.com>
2024-05-03 12:57:18 +0000

    feat: runCheck.ts scaffold add check command for workflow validation

---
+++ src/runCheck.ts
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+
+import { logDiff } from "./differ.js";
+import { Check } from "./workflow/check.js";
+import { Checker } from "./workflow/checker.js";
+import { Merger } from "./workflow/merger.js";
+import { WorkflowResource } from "./workflow/resource.js";
+import { Workflow } from "./workflow/workflow.js";
