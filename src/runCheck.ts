ddf41808bb2067933f79ff407fc54c9a03154399
superfortune <sfortunepassion0908@outlook.com>
2024-05-04 12:49:50 +0000

    feat: runCheck.ts improve add check command for workflow validation

--- src/runCheck.ts
+++ src/runCheck.ts
@@ -6,3 +6,12 @@ import { Checker } from "./workflow/checker.js";
 import { Merger } from "./workflow/merger.js";
 import { WorkflowResource } from "./workflow/resource.js";
 import { Workflow } from "./workflow/workflow.js";
+
+export function logCheck(
+    check: Check,
+    forceUpdate = false,
+    showDiff = false
+): void {
+    if (check.action === "equal") return;
+    if (!forceUpdate && check.force) {
+        return console.log(chalk.grey(`  ${check.noForceMessage}`));
