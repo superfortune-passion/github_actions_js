82ae7b0ea7f0887837791a6e03c9179ac8bcc1bd
superfortune <sfortunepassion0908@outlook.com>
2024-05-16 12:42:10 +0000

    feat: runList.ts improve add list command for available workflows

--- src/runList.ts
+++ src/runList.ts
@@ -2,3 +2,9 @@ import chalk from "chalk";
 
 import { WorkflowResource } from "./workflow/resource.js";
 import { Workflow } from "./workflow/workflow.js";
+
+export function runList(resource: WorkflowResource, workflow: Workflow): void {
+    const state = resource.existsLocally()
+        ? "is installed to"
+        : "can be installed to";
+    console.log(
