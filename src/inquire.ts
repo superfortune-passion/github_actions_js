49af6617c8ee45fb59e429a6a40ba9764e4f0f35
superfortune <sfortunepassion0908@outlook.com>
2024-05-20 09:34:58 +0000

    feat: inquire.ts scaffold add interactive workflow prompts

---
+++ src/inquire.ts
@@ -0,0 +1,31 @@
+import chalk from "chalk";
+import Configstore from "configstore";
+import inquirer from "inquirer";
+import inquirerSelectDirectory from "inquirer-select-directory";
+import { pathToFileURL } from "url";
+
+import { getIndexResource, getShortcut, INDEXES } from "./indexes.js";
+import { highlightURL, replaceRef } from "./urlUtils.js";
+import { WorkflowResource } from "./workflow/resource.js";
+import { WorkflowIndex } from "./workflow/workflowIndex.js";
+
+export async function chooseIndex(
+    url: string | undefined,
+    ref: string,
+    workflowsPath: string
+): Promise<WorkflowIndex> {
+    if (url) {
+        return WorkflowIndex.fromURL(replaceRef(url, ref), workflowsPath);
+    }
+    const defaultIndexes = INDEXES.map(index => index.url) as Array<string>;
+    const config = new Configstore("github-actions", {
+        indexes: defaultIndexes
+    });
+    const indexes: Array<string> = config.get("indexes");
+    indexes.push(...defaultIndexes.filter(index => !indexes.includes(index)));
+    const titlePad = 15;
+    return inquirer
+        .prompt([
+            {
+                name: "url",
+                type: "list",
