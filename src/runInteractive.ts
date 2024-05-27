ee6f6e0c28d1304df378d144f828da9e197f044b
superfortune <sfortunepassion0908@outlook.com>
2024-05-27 12:10:17 +0000

    feat: runInteractive.ts improve add interactive mode orchestration

--- src/runInteractive.ts
+++ src/runInteractive.ts
@@ -13,3 +13,18 @@ import {
 import { getCheckResult, logCheck, runCheck } from "./runCheck.js";
 import { runList, runListAll } from "./runList.js";
 import { logUpdate, runUpdate } from "./runUpdate.js";
+import { highlightURL } from "./urlUtils.js";
+import { Check } from "./workflow/check.js";
+import { WorkflowResource } from "./workflow/resource.js";
+
+async function logWorkflowChecks(
+    resources: Array<WorkflowResource>,
+    args: Namespace
+): Promise<Array<[WorkflowResource, Array<Check>]>> {
+    await Promise.all(resources.map(resource => resource.getRemote()));
+    const checkLists = await Promise.all(
+        resources.map(resource => runCheck(resource, args.force, args.clean))
+    );
+    const result: Array<[WorkflowResource, Array<Check>]> = [];
+    resources.forEach((resource, index) => {
+        const checks = checkLists[index];
