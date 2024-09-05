62a5fb96b45f6d60af9ef8b3087dfc12c5464d07
superfortune <sfortunepassion0908@outlook.com>
2024-09-05 12:25:26 +0000

    feat: utils.test.ts refine add utility function unit tests

--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -13,3 +13,8 @@ test("decapitalize", async () => {
     expect(decapitalize("")).toBe("");
     expect(decapitalize("name")).toBe("name");
     expect(decapitalize("NAME")).toBe("nAME");
+});
+
+test("get version string", () => {
+    expect(getVersionString() === "unknown").toBeFalsy();
+});
