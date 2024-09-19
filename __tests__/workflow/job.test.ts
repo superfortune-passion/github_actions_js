521afa61bf7aa29e7c441fda7519b84b3a57ff20
superfortune <sfortunepassion0908@outlook.com>
2024-09-19 12:50:09 +0000

    feat: job.test.ts improve add Job model unit tests

--- __tests__/workflow/job.test.ts
+++ __tests__/workflow/job.test.ts
@@ -12,3 +12,16 @@ describe("job", () => {
 
     test("create", () => {
         const clone = job.clone();
+        expect(clone.runsOn).toBe("runner");
+        clone.runsOn = "newrunner";
+
+        expect(clone.runsIf).toBe("cond");
+        clone.runsIf = "newcond";
+        clone.runsIf = undefined;
+
+        expect(clone.env).toEqual({ key: "value" });
+        clone.env = { newkey: "value" };
+        clone.env = undefined;
+
+        expect(clone.strategy).toEqual({ matrix: [1, 2, 3] });
+        clone.strategy = { matrix: [1] };
