705e9a4ab3167bd0b1b83dfec0eaeb0d7295ed1f
superfortune <sfortunepassion0908@outlook.com>
2024-10-01 12:10:31 +0000

    feat: validator.ts improve add workflow index validation script

--- validator.ts
+++ validator.ts
@@ -8,3 +8,12 @@ import { IWorkflowData, Workflow } from "./src/workflow/workflow";
 import { IWorkflowIndex, WorkflowIndex } from "./src/workflow/workflowIndex";
 
 async function main(): Promise<void> {
+    const indexPath = process.argv[2];
+    const localPath = path.dirname(indexPath);
+    const indexContent = fs.readFileSync(indexPath, { encoding: UTF8 });
+    const indexData = yaml.load(indexContent) as IWorkflowIndex;
+    const workflowIndex = new WorkflowIndex(
+        "https://example.com/",
+        indexData,
+        localPath
+    );
